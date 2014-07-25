671cb49539c8ede1f476037a35535dea09e7eb4a
Daniel Barbalato <dan.barbalato@braintrust-holdings.com>
2014-07-25 14:26:31 -0400

    Fixed issues with copyright headers

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@
 #
 # This file is part of REDHAWK Basic Components sinksocket.
 # 
-# REDHAWK Basic Components psd is free software: you can redistribute it and/or modify it under the terms of 
+# REDHAWK Basic Components sinksocket is free software: you can redistribute it and/or modify it under the terms of 
 # the GNU General Public License as published by the Free Software Foundation, either 
 # version 3 of the License, or (at your option) any later version.
 # 
-# REDHAWK Basic Components psd is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+# REDHAWK Basic Components sinksocket is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
 # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
 # PURPOSE.  See the GNU General Public License for more details.
 # 
